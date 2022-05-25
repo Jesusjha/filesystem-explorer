3341210dd58276ff47049aa7b4ccdccfc58b2779
Jesus Herrera <heisusin@gmail.com>
2022-05-25 02:17:31 +0200

    trying to upload files

---
+++ add-file.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['upload'])) {
+    
+    $file = $_FILES['file'];
+    print_r($file);
+    $fileName = $_FILES['file']['name'];
+    $fileTmp = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode(".", $fileName);
+    $fileOnlyExt = strtolower(end($fileExt));
+
+}
