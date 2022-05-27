66b9683531d0e658f582ef6c3da617a3e19ff190
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-05-27 16:26:43 +0200

    delete file or directory added

--- remove-directories.php
+++ remove-directories.php
@@ -1,28 +1,17 @@
 <?php
 
-session_start();
 
-function deleteAll($root, $delFile) {
-    if ($_POST) {
-        foreach(glob($delFile) as $file) {
-        if(is_dir($file)){
-            deleteAll($file);
+function deleteDirectory($path){
+    if(is_dir($path)){
+        $files = glob($path . '/*');
+        foreach($files as $file){
+            deleteDirectory($file);
         }
-        else
-        unlink($file);
-        }
-        rmdir($root);
+        rmdir($path);
+    } else {
+        unlink($path);
     }
     header("Location: ./index.php");
 }
 
-$delFile = './root/ati-cachorro.jpg';
-
-$folderName= $_POST['delete'];
-$fileDelName = $_POST['delete'];
-$_SESSION['folderName'] = $folderName;
-
-deleteAll("./root/$folderName", "./root/$fileDelName");
-
-
-
+deleteDirectory("./root/".$_POST['delete']);
