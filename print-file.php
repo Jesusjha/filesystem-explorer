1b1325ec1b49e2587ce3fb8ec6e992b5707449ef
Jesus Herrera <heisusin@gmail.com>
2022-05-30 15:37:05 +0200

    added show icon extension

--- print-file.php
+++ print-file.php
@@ -1,16 +1,25 @@
 <?php
+include('show-icon.php');
+
 $folderName = $_POST['folder'];
 
 if ($_FILES){
     $dir = "./root/$folderName" . "/";
     $path = $dir . $_FILES['fileInFolder']['name'];
+    $name = $_FILES['fileInFolder']['name'];
+    $fileExt = explode('.',$name);
+    $fileActExt = strtolower(end($fileExt));
     
+
+
+
+            
     if (!file_exists($dir)) {
         mkdir($dir, 0777);
     }
     
     if(move_uploaded_file($_FILES['fileInFolder']['tmp_name'], $path)) {
-        header ('Location: index.php');
+        #header ('Location: index.php');
     } 
     
 }
@@ -20,22 +29,24 @@ $dirs = array_slice(scandir("./root/" . $folderName), 2);
 //loop through the array and print the names of the directories
 if (count($dirs) > 0) {
     foreach($dirs as $pos => $dir){
-       echo "<div class='container-fluid'>
-       <div class='card shadow-none border radius-15'>
-           <div class='card-body'>
-               <div class='d-flex'>
-                   <div class='font-30 text-primary'><i class='bx bxs-folder'></i>
-                   </div>
-                   <div class='user-plus'></div>
-               </div>
-               <h6 class='mb-0 text-primary'>$dir</h6>
-           </div>
-           <form action='delete-file.php' method='post'>
-           <input type='hidden' name='parentFolder' value='$folderName'>
-           <button type='submit' name='deleteFile' value='$dir' class='dropdown-item' href='#'><i class='bx bxs-trash'></i></button>
-           </form>
-       </div>
-   </div>";
+        // $src = getIcons($fileActExt);
+    echo "<div class='container-fluid'>
+    <div class='card shadow-none border radius-15'>
+        <div class='card-body'>
+            <div class='d-flex'>
+                <div class='font-30 text-primary'>
+                <img src=" . getIcons($fileActExt) .">
+                </div>
+                <div class='user-plus'></div>
+            </div>
+            <h6 class='mb-0 text-primary'>$dir</h6>
+        </div>
+        <form action='delete-file.php' method='post'>
+        <input type='hidden' name='parentFolder' value='$folderName'>
+        <button type='submit' name='deleteFile' value='$dir' class='dropdown-item' href='#'><i class='bx bxs-trash'></i></button>
+        </form>
+    </div>
+</div>";
     }
 }
 ?>
